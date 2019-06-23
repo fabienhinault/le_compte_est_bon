2181d5bacc7fbe0244073b12fd0bec77efde2a07
Fabien <fabien.hinault@free.fr>
2019-06-23 20:51:09 +0200

    +doc

--- le_compte_est_bon.rkt
+++ le_compte_est_bon.rkt
@@ -2,7 +2,7 @@
 
 
 (define-syntax-rule (let1 a b body ...)
-(let ((a b)) body ...))
+  (let ((a b)) body ...))
 
 (require rackunit)
 
@@ -17,14 +17,23 @@
 
 ; make an accumulator for fonction h
 ; 2 -> '((2) (2 ()))
+; The first list is the working space where all the number are stored.
+; The following ones will contain a number and the list of all other numbers.
+; Hence, for the first time: x, and the empty list.
 (define (make-acc x)
-  (list (list x) (list x '())))
+  `((,x) (,x ())))
 
+(check-equal? (make-acc 2) '((2) (2 ())))
+
+; add a new number x to the accumulator
+; add it to the first list = (car acc) = working space
+; add the new list of x and the list of other numbers
+; add x to the previous lists, as other number
 (define (add-to-acc x acc)
   (list* (cons x (car acc))
-        (list x (car acc))
+         (list x (car acc))
          (map (lambda (_) (list (car _) (cons x (cadr _))))
-               (cdr acc))))
+              (cdr acc))))
 
 (define (g l acc)
   (if (null? l)
@@ -37,6 +46,6 @@
 (define lceb (h nbs))
 
 (map (lambda (_)
-         (list (list 10 (car _))
-               (cadr _)))
-       (h (cadar lceb)))
+       (list (list 10 (car _))
+             (cadr _)))
+     (h (cadar lceb)))
